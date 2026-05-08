e8855ce234b8c3b2abdf094f69756a92ee2b3312
Sara <saravaldes.0719@gmail.com>
2026-05-07 19:26:37 -0500

    7 de mayo

--- src/app/components/animal-component/animal-component.ts
+++ src/app/components/animal-component/animal-component.ts
@@ -1,25 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { AnimalService } from '../../services/animal-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-animal-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './animal-component.html',
   styleUrl: './animal-component.css',
 })
 export class AnimalComponent {
   animalList: any = [];
 
-  constructor(private animalService: AnimalService) { }
+  constructor(private animalService: AnimalService, private cd: ChangeDetectorRef) { }
 
   getAllAnimals() {
     this.animalService.getAllAnimalsData().subscribe((data: {}) => {
+      
       this.animalList = data;
-      console.log(this.animalList);
+      console.log(this.animalList)
+      this.cd.detectChanges();
     });
   }
   ngOnInit() {
     this.getAllAnimals();
   }
-
+  ngOnChanges() {
+    this.getAllAnimals();
+  }
 }
